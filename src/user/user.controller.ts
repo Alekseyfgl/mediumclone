cd330828ea570806546e27bcff10f259cc98c420
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-05-13 23:58:53 +0300

    add pipes (Validation)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserResponseInterface } from '@app/user/types/userResponse.interface';
@@ -9,6 +9,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
+  @UsePipes(new ValidationPipe()) // проверяет наш body и проверяет что мы передали в createUserDto
   async createUser(
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserEntity } from '@app/user/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from '@app/config';
-import { UserResponseInterface } from "@app/user/types/userResponse.interface";
+import { UserResponseInterface } from '@app/user/types/userResponse.interface';
 
 @Injectable()
 export class UserService {
@@ -16,6 +16,23 @@ export class UserService {
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     // лучше явно указываать что мы хотим вернуть
+
+    const userByEmail = await this.userRepository.findOne({
+      email: createUserDto.email, // делаем запрос в BD и проверяем есть ли такой пользователь c email в BD
+    });
+
+    const userByName = await this.userRepository.findOne({
+      email: createUserDto.email, // делаем запрос в BD и проверяем есть ли такой пользователь name в BD
+    });
+
+    //вывод ошибки если есть пользователь с таким email | name
+    if (userByEmail || userByName) {
+      throw new HttpException(
+        'Email or username are taken',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
     const newUser = new UserEntity();
 
     Object.assign(newUser, createUserDto);
